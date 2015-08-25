a26a8391a06a8a3712aea6c0d14f7d47d77c91a7
dw443 <dw443@users.noreply.github.com>
2015-08-25 01:35:32 -0700

    Update script.js
    Added some class banner jquery

--- www/script.js
+++ www/script.js
@@ -1,1 +1,12 @@
+var main = function() {
+	$(".banner").hover(
+        function() {
+    		$(this).fadeTo('fast',1);
+  		}, 
+        function() {
+    		$( this ).fadeTo('fast',0.5);
+  		}
+    );
+};
 
+$(document).ready(main);
